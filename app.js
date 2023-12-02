ce6a3f252e830582f49edfb017619dfaf930c11c
MdFazilGITHUB <mjam7221jmi@gmail.com>
2023-12-02 12:04:50 +0530

    added comments

--- app.js
+++ app.js
@@ -1,53 +1,66 @@
+// Get the search input element
 let searchValue = document.getElementById("searchValue");
 
+// Add an event listener to the search input for the Enter key
 searchValue.addEventListener("keydown", function (event) {
   if (event.keyCode === 13) {
+    // If Enter is pressed, call the getMealCategory function
     getMealCategory();
   }
 });
 
+// Async function to fetch meal categories based on the user input
 async function getMealCategory() {
   try {
+    // Get elements for displaying meals and empty message
     let gridContainer = document.getElementById("gridContainer");
     let empty = document.getElementById("empty")
     let card = "";
+
+    // Make a GET request using axios to fetch meal data based on the category
     let response = await axios.get(
       `https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchValue.value}`
     );
+    
+    // Extract data from the response
     let resData = await response.data;
     let meals = resData.meals;
 
+    // Check if meals are found
     if (meals && meals.length > 0) {
+      // Loop through each meal and create HTML cards
       for (let i = 0; i < meals.length; i++) {
-        // console.log("meals.strMeal: ", meals[i].strMeal);
         card += `<div id="gridBlock" onclick="showIngredients('${meals[i].strMeal}')"  data-aos="fade-up" data-aos-duration="2000">
-                <h2 id="name" data-aos="fade-up">${meals[i].strMeal}</h2>
-                <div id="mealImage">
-                  <img src=${meals[i].strMealThumb}  alt="" id="image" data-aos="zoom-in" data-aos-duration="2000">
-                </div>
-              </div>`;
+                  <h2 id="name" data-aos="fade-up">${meals[i].strMeal}</h2>
+                  <div id="mealImage">
+                    <img src=${meals[i].strMealThumb}  alt="" id="image" data-aos="zoom-in" data-aos-duration="2000">
+                  </div>
+                </div>`;
       }
 
+      // Check if any cards were generated
       if (card !== "") {
+        // If cards are generated, display them in the gridContainer and hide the empty message
         gridContainer.innerHTML = card;
-        empty.innerHTML = ""
-      } 
-      else {
-        empty.style.display = "block"
+        empty.innerHTML = "";
+      } else {
+        // If no cards were generated, display a message in the empty div
+        empty.style.display = "block";
         empty.innerHTML ="Not Found (please check the category again)";
       }
-    } 
-    else {
-      gridContainer.innerHTML = ""
-      empty.innerHTML =
-      "Not Found (please check the category entered again)";
-      empty.style.display = "block"
-      }
-    } catch (error) {
+    } else {
+      // If no meals are found, display a message in the empty div
+      gridContainer.innerHTML = "";
+      empty.innerHTML ="Not Found (please check the category entered again)";
+      empty.style.display = "block";
+    }
+  } catch (error) {
+    // Handle any errors that occur during the fetch
     console.error("Error fetching data:", error);
   }
 }
 
+// Get elements for the modal
 let modal = document.getElementById("myModal");
 let modalTitle = document.getElementById("modalTitle");
 let ingredientsList = document.getElementById("ingredientsList");
@@ -55,11 +68,13 @@ let closeModal = document.getElementsByClassName("close")[0];
 let recipe = document.getElementById("recipe")
 let video = document.getElementById("videoLink")
 
+// Function to show ingredients when a meal is clicked
 function showIngredients(event) {
   let mealId = event;
   getMealDetails(mealId);
 }
 
+// Async function to fetch meal details based on the meal ID
 async function getMealDetails(mealId) {
   try {
     let response = await axios.get(
@@ -68,12 +83,14 @@ async function getMealDetails(mealId) {
     let dataRes = await response.data;
     let mealDetails = dataRes.meals;
 
+    // Set modal content
     modalTitle.innerText = mealDetails[0].strMeal;
     ingredientsList.innerHTML = createIngredientsList(mealDetails);
     recipe.innerText = getRecipe(mealDetails)
     video.onclick = () => {
       videoReroute(mealDetails) 
     }
+
     // Show the modal
     modal.style.display = "block";
   } catch (err) {
@@ -81,7 +98,7 @@ async function getMealDetails(mealId) {
   }
 }
 
-
+// Function to create an ingredients list HTML
 function createIngredientsList(mealDetails) {
   let ingredients = [];
   for (let i = 1; i <= 20; i++) {
@@ -95,20 +112,22 @@ function createIngredientsList(mealDetails) {
   return ingredients.join("");
 }
 
+// Function to get the recipe text from meal details
 function getRecipe(mealDetails){
   return mealDetails[0].strInstructions
-  
 }
 
+// Function to redirect to the YouTube video link
 function videoReroute(mealDetails){
   window.location.href = mealDetails[0].strYoutube
 }
 
+// Event listener for closing the modal
 closeModal.onclick = function () {
   modal.style.display = "none";
 };
 
-
+// Event listener to close the modal if clicked outside the modal content
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
--- randomMeal.js
+++ randomMeal.js
@@ -1,19 +1,23 @@
+// Get elements for the random meal display
 let randomName = document.getElementById("randomName");
 let randomImage = document.getElementById("randomImage");
 let Title = document.getElementById("modalTitle");
 
+// Function to fetch and display a random meal
 async function getRandomMeal() {
   try {
+    // Make a GET request using axios to fetch a random meal
     let response = await axios.get(
       "https://www.themealdb.com/api/json/v1/1/random.php"
     );
     let dataRes = await response.data;
     let randomMeal = dataRes.meals[0];
 
+    // Display the random meal name and image
     randomName.innerText = randomMeal.strMeal;
     randomImage.src = randomMeal.strMealThumb;
 
-    // Pass randomMeal data to the ingredients function using a closure
+    // Attach a click event to the random image to show ingredients
     randomImage.onclick = function () {
       ingredients(randomMeal);
     };
@@ -22,63 +26,68 @@ async function getRandomMeal() {
   }
 }
 
-getRandomMeal()
+// Call getRandomMeal to initially display a random meal
+getRandomMeal();
 
+// Function to show ingredients of a random meal
 function ingredients(randomMeal) {
-    if (randomMeal) {
-      let mealId = randomMeal.strMeal;
-      getRandomMealDetails(randomMeal);
-    } else {
-      console.error("Invalid randomMeal data");
-    }
+  if (randomMeal) {
+    let mealId = randomMeal.strMeal;
+    getRandomMealDetails(randomMeal);
+  } else {
+    console.error("Invalid randomMeal data");
   }
-  
-  function getRandomMealDetails(randomMeal) {
-    Title.innerText = randomMeal.strMeal;  // Fix here
-    ingredientsList.innerHTML = createRandomIngredientsList(randomMeal);
-    recipe.innerText = getRandomRecipe(randomMeal);
-    video.onclick = () => {
-      videoRandomReroute(randomMeal);
-    };
-    // Show the modal
-    modal.style.display = "block";
 }
-  
-  function createRandomIngredientsList(randomMeal) {
-    let ingredients = [];
-    for (let i = 1; i <= 20; i++) {
-      // Check if the ingredient exists and is not an empty string
-      if (
-        randomMeal[`strIngredient${i}`] &&
-        randomMeal[`strIngredient${i}`].trim() !== ""
-      ) {
-        ingredients.push(
-          `<li>${randomMeal[`strMeasure${i}`]} ${
-            randomMeal[`strIngredient${i}`]
-          }</li>`
-        );
-      }
-    }
-    return ingredients.join("");
-  }
-  
-  function getRandomRecipe(randomMeal) {
-    return randomMeal.strInstructions;
-  }
-  
-  function videoRandomReroute(randomMeal) {
-    window.location.href = randomMeal.strYoutube;
-  }
-  
-  closeModal.onclick = function () {
-    modal.style.display = "none";
-  };
-  
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
-  
-  
 
+// Function to display details of a random meal
+function getRandomMealDetails(randomMeal) {
+  Title.innerText = randomMeal.strMeal;
+  ingredientsList.innerHTML = createRandomIngredientsList(randomMeal);
+  recipe.innerText = getRandomRecipe(randomMeal);
+  video.onclick = () => {
+    videoRandomReroute(randomMeal);
+  };
+  // Show the modal
+  modal.style.display = "block";
+}
+
+// Function to create an ingredients list HTML for a random meal
+function createRandomIngredientsList(randomMeal) {
+  let ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    // Check if the ingredient exists and is not an empty string
+    if (
+      randomMeal[`strIngredient${i}`] &&
+      randomMeal[`strIngredient${i}`].trim() !== ""
+    ) {
+      ingredients.push(
+        `<li>${randomMeal[`strMeasure${i}`]} ${
+          randomMeal[`strIngredient${i}`]
+        }</li>`
+      );
+    }
+  }
+  return ingredients.join("");
+}
+
+// Function to get the recipe text for a random meal
+function getRandomRecipe(randomMeal) {
+  return randomMeal.strInstructions;
+}
+
+// Function to redirect to the YouTube video link for a random meal
+function videoRandomReroute(randomMeal) {
+  window.location.href = randomMeal.strYoutube;
+}
+
+// Event listener for closing the modal
+closeModal.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Event listener to close the modal if clicked outside the modal content
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
